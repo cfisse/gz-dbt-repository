ae10e69122d5cf2a953e9b6a7d4914d735044107
constamax@gmail.com <constamax@gmail.com>
2023-11-01 09:54:57 +0000

    sales margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,12 @@
-SELECT * 
-FROM {{ source('raw', 'sales') }} 
-JOIN {{ source('raw', 'product') }}
-USING (pdt_id)
+SELECT 
+    products_id, 
+    date_date,
+    orders_id,
+    revenue,
+    quantity,
+    purchase_price,
+    ROUND(CAST(s.quantity AS FLOAT64) * CAST(p.purchase_price AS FLOAT64), 2) AS purchase_cost,
+    s.revenue - ROUND(CAST(s.quantity AS FLOAT64) * CAST(p.purchase_price AS FLOAT64), 2) AS margin
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p
+        USING (products_id)
